9d09773626a576f8dab9890f9d2e09ce06843391
GlendyT <glendytuyuc@gmail.com>
2024-06-05 18:34:59 -0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
+import { BoardComponent } from './board/board.component';
 import { RouterOutlet } from '@angular/router';
+import { GameService } from './game.service';
+import { NgClass, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, BoardComponent, NgIf, NgClass],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'tiktaktoe';
+  constructor (public gameService: GameService){ }
+
+  resetGame() {
+    this.gameService.newGame();
+  }
 }
---
+++ src/app/board/board.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { SquareComponent } from '../square/square.component';
+import { GameService } from '../game.service';
+import { NgFor } from '@angular/common';
+
+@Component({
+  selector: 'app-board',
+  standalone: true,
+  imports: [SquareComponent, NgFor],
+  templateUrl: './board.component.html',
+  styleUrl: './board.component.css'
+})
+export class BoardComponent implements OnInit {
+
+  constructor (public gameService: GameService) {
+
+  }
+
+  ngOnInit(): void {}
+
+}
